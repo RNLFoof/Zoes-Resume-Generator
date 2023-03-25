018f9c5295d98ef9809b30cfa0afbc137ce9d2df
RNLFoof <z.zablotsky@gmail.com>
2023-03-25 14:20:36 -0400

    Basic format transition.

---
+++ classes/Impressiveness.py
@@ -0,0 +1,36 @@
+from enum import Enum
+
+
+class Impressiveness(Enum):
+    def __init__(self, number, description):
+        self.number = number
+        self.description = description
+
+    NONE = 0, "Useless garbage"
+
+    SIMPLE = 1, """ 
+        Could be learned in its entirety in a day and has no real edges
+        Ex. JSON
+  """
+
+    INTUITIVE = 1.5, """
+        Has enough going on that you couldn't trivially memorize all of it, but you aren't going to have any trouble learning how to do new things.
+        Ex. Google Calendar, Hue Manager
+    """
+
+    INTUITIVE_WITH_CONTEXT = 1.75, """     
+        Same as intuitive, but only if you have prior knowledge about some other thing.
+        Ex. MSAL, which would be confusing if you knew nothing about authorization.
+    """
+
+    BEGINNER = 2, """     
+        Deliberately simplified at the expense of otherwise expected functionality, but there's a fair bit you can do with it.
+        Intended for people who don't know how to use better but more complicated tools.
+        Ex. GML, Power Automate
+    """
+
+    DEEP = 3, "Has depth"
+
+    MAKE_OR_BREAK = 4, "Could be a make or break skill for a position"
+
+    SPECIALIZABLE = 5, "Somebody could specialize in this"
