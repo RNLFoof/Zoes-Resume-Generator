33f79a9f2e4e12f5af1d53b6bfed762fcf9cdb47
RNLFoof <z.zablotsky@gmail.com>
2023-03-30 09:05:49 -0400

    Fixed the import.

--- classes/Skillset.py
+++ classes/Skillset.py
@@ -4,7 +4,7 @@ from typing import Callable
 import json5
 from pydantic import BaseModel
 
-from Impressiveness import Impressiveness
+from classes.Impressiveness import Impressiveness
 
 SAVED_TO = os.path.join(
     os.path.abspath(os.path.split(__file__)[0]),
