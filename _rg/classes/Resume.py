2b99b87a0e1650cd3fe431d6ff28ff9fcf156755
RNLFoof <z.zablotsky@gmail.com>
2023-04-25 14:22:54 -0400

    Made skills into a table

--- _rg/classes/Resume.py
+++ _rg/classes/Resume.py
@@ -1,12 +1,8 @@
-import re
+import subprocess
+from functools import cache
 from typing import Any
 
-from pylatex import Document, Section, Package, Command
-from pdflatex import PDFLaTeX
-import os
-from functools import cache
-import subprocess
-
+from _rg.classes.History import history
 from _rg.classes.SkillSet import SkillSet
 
 
@@ -14,10 +10,10 @@ class Resume:
     @cache
     def tex(self):
         skill_set = SkillSet.all()
-        skills = skill_set.skills_by_generic_value()
 
         global_variables = {
-            "skills": "\n\n".join([skill.name for skill in skills])
+            "skills": skill_set.tex(),
+            "history": history.tex(),
         }
 
         s = ""
@@ -39,7 +35,7 @@ class Resume:
         with open(fr"tex/{section_name}.tex", "r") as f:
             s = f.read() + "\n"
         for name, value in variables.items():
-            s = s.replace(f"__{name}__", self.tex_escape(value))
+            s = s.replace(f"__{name}__", value)
         return s
 
     def generate_tex(self):
@@ -49,11 +45,3 @@ class Resume:
     def generate_pdf(self):
         self.generate_tex()
         subprocess.run(["pdflatex", "-output-directory=out", "out/resume.tex"])
-
-    @staticmethod
-    def tex_escape(text: str) -> str:
-        text = text.replace("\\", r"\textbackslash")
-        text = re.sub(r"([&%$#_{}])", r"\\\1", text)
-        text = text.replace("~", r"\textasciitilde")
-        text = text.replace("^", r"\textasciicircum")
-        return text
