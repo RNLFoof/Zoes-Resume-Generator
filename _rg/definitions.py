e062a20c7528a434ecf1078cb0b6eaef7c93e1c0
RNLFoof <z.zablotsky@gmail.com>
2023-05-16 18:01:05 -0400

    chore: added definitions.TEST_OUTPUT_DIR

--- _rg/definitions.py
+++ _rg/definitions.py
@@ -2,3 +2,4 @@ import os
 
 ROOT_DIR = os.path.abspath(os.path.split(__file__)[0])  # https://stackoverflow.com/a/25389715
 IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+TEST_OUTPUT_DIR = os.path.join(ROOT_DIR, "../tests/out")
